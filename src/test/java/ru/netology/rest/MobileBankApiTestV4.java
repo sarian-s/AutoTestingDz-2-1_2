f4da6a597145c51b3852dfd3139de5ceb5a6c658
sergey s <som-ana@yandex.ru>
2023-06-24 19:18:04 +0300

    added no error test

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -17,7 +17,7 @@ class MobileBankApiTestV4 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
